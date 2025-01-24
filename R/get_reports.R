1b1a1d76488e9aa3695100776193979a0cda4392
jspowley <jpowley@ualberta.ca>
2025-01-23 20:25:10 -0700

    bugfix in python but another bug still exists!

--- R/get_reports.R
+++ R/get_reports.R
@@ -7,8 +7,12 @@
 #'
 
 get_reports <- function(url){
-  py_install("pandas openpyxl")
-  py_run_string("
+  reticulate::py_install("pandas")
+  reticulate::py_install("openpyxl")
+  # Why Python? No JS dependency or out of date, broken xls package this way.
+  # This is one of the 10% of use cases where python is better,
+  # since the file type is niche and Python has the wider user base needed to keep the parser supported.
+  reticulate::py_run_string("
 import pandas as pd
 
 def data(url):
@@ -24,7 +28,7 @@ def data(url):
     combined_df = pd.concat(dfs, ignore_index=True)
     return combined_df
 ")
-  combined_df_py <- py$data(url)
+  combined_df_py <- reticulate::py$data(url)
   combined_df_r <- reticulate::py_to_r(combined_df_py) %>%
     return()
 }
