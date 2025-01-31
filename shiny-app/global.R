01cfbde68153435b6229d627280c76864b50b1c4
jspowley <jpowley@ualberta.ca>
2025-01-31 16:28:35 -0700

    added library requirements

--- shiny-app/global.R
+++ shiny-app/global.R
@@ -1,4 +1,5 @@
-
+library(eiatools)
+library(tidyverse)
 ## TESTS
 my_global_var_test <- "Data Connection To eia Will Be Done On This Page"
 #data_index_test <- dindex_get_data(data_index[["steo"]][["seriesDescription"]], api_key, "2024-01-01",  "2025-01-01") ## Error Message
